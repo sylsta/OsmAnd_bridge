792c3459f8dd9a56b436b19e938a8ddb9a7989df
Sylvain Théry <sylvain.thery@cnrs.fr>
2022-11-21 23:23:05 +0100

    new class name

--- __init__.py
+++ __init__.py
@@ -32,5 +32,5 @@ def classFactory(iface):  # pylint: disable=invalid-name
     :type iface: QgsInterface
     """
     #
-    from .OsmAnd_bridge import OsmAndLinker
-    return OsmAndLinker(iface)
+    from .OsmAnd_bridge import OsmAndBridge
+    return OsmAndBridge(iface)
