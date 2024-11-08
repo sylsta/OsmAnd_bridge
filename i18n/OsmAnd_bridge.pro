c73513c791119023960a4131b004e2ac45eff363
Sylvain Théry <sylvain.thery@cnrs.fr>
2024-11-08 20:17:14 +0100

    Translation update

--- i18n/OsmAnd_bridge.pro
+++ i18n/OsmAnd_bridge.pro
@@ -1,3 +1,3 @@
 FORMS = ../OsmAnd_bridge_import_dialog.ui
-SOURCES = ../OsmAnd_bridge_geopackage_management.py ../OsmAnd_bridge_import_dialog.py ../OsmAnd_bridge_import_process.py ../OsmAnd_bridge.py
+SOURCES = ../dev_generate_annotation_from_ui_for_completion.py ../mtp_crashtests4linux.py ../OsmAnd_bridge_geopackage_management.py ../OsmAnd_bridge_import_dialog.py ../OsmAnd_bridge_import_process.py ../OsmAnd_bridge.py
 TRANSLATIONS = OsmAnd_bridge_fr.ts
--- i18n/OsmAnd_bridge_fr.ts
+++ i18n/OsmAnd_bridge_fr.ts
@@ -24,42 +24,42 @@
         <translation>Notes audiovisuelles</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="146"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="156"/>
         <source>Open file</source>
         <translation>Ouverture du fichier</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="178"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="188"/>
         <source>Waypoints</source>
         <translation>Waypoints</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="178"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="188"/>
         <source>Routes</source>
         <translation>Routes</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="178"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="188"/>
         <source>Tracks</source>
         <translation>Traces</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="179"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="189"/>
         <source>Route points</source>
         <translation>Points de routes</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="179"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="189"/>
         <source>Track points</source>
         <translation></translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="207"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="217"/>
         <source>favorites</source>
         <translation>Favoris</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_process.py" line="209"/>
+        <location filename="../OsmAnd_bridge_import_process.py" line="219"/>
         <source>Itinerary</source>
         <translation>Marques</translation>
     </message>
@@ -67,49 +67,74 @@
 <context>
     <name>Dialog</name>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.ui" line="14"/>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="17"/>
         <source>OsmAnd bridge - Select items to import </source>
         <translation>OsmAnd bridge - Sélectionnez les liens à importer </translation>
     </message>
     <message>
         <location filename="../OsmAnd_bridge_import_dialog.ui" line="120"/>
         <source>favorites</source>
-        <translation>Favoris</translation>
+        <translation type="obsolete">Favoris</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.ui" line="142"/>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="145"/>
         <source>Itinerary</source>
         <translation>Marques</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.ui" line="161"/>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="164"/>
         <source>AVnotes</source>
         <translation>Notes AV</translation>
     </message>
     <message>
         <location filename="../OsmAnd_bridge_import_dialog.ui" line="174"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select the&lt;a href=&apos;https://osmand.net/&apos; tagert=&apos;_blank&apos;&gt; OsmAnd &lt;/a&gt;file directory of your phone:</source>
-        <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Sélectionnez le répertoire &lt;a href=&apos;https://osmand.net/&apos; tagert=&apos;_blank&apos;&gt; OsmAnd &lt;/a&gt;de votre téléphone :</translation>
+        <translation type="obsolete">&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Sélectionnez le répertoire &lt;a href='https://osmand.net/' tagert='_blank'&gt; OsmAnd &lt;/a&gt;de votre téléphone :</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.ui" line="190"/>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="193"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select the track(s) you want to download:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Sélectionnez la/les  trace(s)  que vous voulez télécharger :&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.ui" line="203"/>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="206"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select the point feature(s) you want to download:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Sélectionnez les données ponctuelles que vous voulez télécharger :&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.ui" line="216"/>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="219"/>
         <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Choose the destination folder on your computer:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
         <translation>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Choisissez le répertoire de destination sur votre ordinateur:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
     </message>
     <message>
         <location filename="../OsmAnd_bridge_import_dialog.ui" line="229"/>
         <source>&lt;br/&gt;&lt;/span&gt;(default is : /Android/data/net.osmand/files&lt;br/&gt;or /Android/data/net.osmand.plus/files on android phones.&lt;br/&gt;You might have to copy this folder on your computer.)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
-        <translation>&lt;br/&gt;&lt;/span&gt;(le répertoire par défaut est : /Android/data/net.osmand/files&lt;br/&gt;ou /Android/data/net.osmand.plus/files sur les téléphones android.&lt;br/&gt;Il vous faudra peut-être copier ce répertoire sur votre ordinateur.)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+        <translation type="obsolete">&lt;br/&gt;&lt;/span&gt;(le répertoire par défaut est : /Android/data/net.osmand/files&lt;br/&gt;ou /Android/data/net.osmand.plus/files sur les téléphones android.&lt;br/&gt;Il vous faudra peut-être copier ce répertoire sur votre ordinateur.)&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="123"/>
+        <source>Favourites</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="177"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select the &apos;file&apos; directory on you computer:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="268"/>
+        <source>Device</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="284"/>
+        <source>Local directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.ui" line="310"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Import &lt;/span&gt;&lt;a href=&quot;https://osmand.net/&quot;&gt;&lt;span style=&quot; font-weight:600; text-decoration: underline; color:#1d99f3;&quot;&gt;OsmAnd&lt;/span&gt;&lt;/a&gt;&lt;span style=&quot; font-weight:600;&quot;&gt; data from...&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
@@ -171,66 +196,96 @@
     </message>
     <message>
         <location filename="../OsmAnd_bridge.py" line="354"/>
-        <source>âªâª This is the End, my only friend, the End âªâª</source>
-        <translation></translation>
+        <source>&#xe2;&#x99;&#xaa;&#xe2;&#x99;&#xaa; This is the End, my only friend, the End &#xe2;&#x99;&#xaa;&#xe2;&#x99;&#xaa;</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 <context>
     <name>OsmAndBridgeImportDialog</name>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="74"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="93"/>
         <source>Name</source>
         <translation>Nom</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="74"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="93"/>
         <source>Size</source>
         <translation>Taille</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="74"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="93"/>
         <source>Last Modified</source>
         <translation>Dernière modification</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="104"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="161"/>
         <source>not valid OsmAnd file path.</source>
         <translation>Chemin du réperrtoire OSM invalide.</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="110"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="167"/>
         <source>no valid OsmAnd tracks path.</source>
         <translation>Chemin des traces OsmAnd invalide.</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="122"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="179"/>
         <source>no gpx file to import.</source>
         <translation>Aucun fichier gpx à importer.</translation>
     </message>
     <message>
         <location filename="../OsmAnd_bridge_import_dialog.py" line="128"/>
         <source>found ./favorites.gpx.</source>
-        <translation>fichier  ./favorites.gpx détecté.</translation>
+        <translation type="obsolete">fichier  ./favorites.gpx détecté.</translation>
     </message>
     <message>
         <location filename="../OsmAnd_bridge_import_dialog.py" line="146"/>
         <source>./favorites.gpx not found.</source>
-        <translation>fichier ./favorites.gpx introuvable.</translation>
+        <translation type="obsolete">fichier ./favorites.gpx introuvable.</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="141"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="198"/>
         <source>found ./itinerary.gpx.</source>
         <translation>fichier ./itinerary.gpx trouvé.</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="153"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="210"/>
         <source>no valid OsmAnd avnotes path.</source>
-        <translation>Chemi des notes AV d&apos;OsmAnd invalide.</translation>
+        <translation>Chemi des notes AV d'OsmAnd invalide.</translation>
     </message>
     <message>
-        <location filename="../OsmAnd_bridge_import_dialog.py" line="165"/>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="222"/>
         <source>no avnote file to import.</source>
         <translation>Aucun fichier de note AV à importer.</translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="127"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select the &apos;file&apos; directory on you computer:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="134"/>
+        <source>&lt;html&gt;&lt;head/&gt;&lt;body&gt;&lt;p&gt;&lt;span style=&quot; font-weight:600;&quot;&gt;Select your device from the list:&lt;/span&gt;&lt;/p&gt;&lt;/body&gt;&lt;/html&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="149"/>
+        <source>not valid output file path.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="185"/>
+        <source>found favorites.gpx.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="190"/>
+        <source>No favorites found.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="../OsmAnd_bridge_import_dialog.py" line="203"/>
+        <source>./itinerary.gpx not found.</source>
+        <translation type="unfinished"></translation>
     </message>
 </context>
 </TS>
