2903b0f31aaec50d5eeb8cd78b9611ce1c227e71
Sylvain Théry <sylvain.thery@cnrs.fr>
2024-11-11 21:44:45 +0100

    refactor name

--- dev_mtp_crashtests4linux.py
+++ dev_mtp_crashtests4linux.py
@@ -28,19 +28,19 @@ for device in devices:
     print(f'{device_open.get_model_name()} - {str(device_open)[9:-2]}')
     device_open.close()
 
-#
-# try:
-#     device = mtpy.get_raw_devices()[0].open()
-# except:
-#     print('Unable to connect to the device. Try disconnecting and reconnecting. Check that it is unlocked.')
-#     exit(-1)
-#
+
+try:
+    device_open = mtpy.get_raw_devices()[0].open()
+except:
+    print('Unable to connect to the device. Try disconnecting and reconnecting. Check that it is unlocked.')
+    exit(-1)
+
 print('Looking for Osmand files')
 potential_paths = ['/Android/data/net.osmand/files', '/Android/obb/net.osmand/files',
                    '/Android/data/net.osmand.plus/files','/Android/obb/net.osmand.plus/files']
 path_found = False
 for path in potential_paths:
-    if device.get_descendant_by_path(path) is not None:
+    if device_open.get_descendant_by_path(path) is not None:
         path_found = True
         break
 if not path_found:
@@ -54,7 +54,7 @@ for item in items_list:
     print(path+item)
     try:
         # copy item to tmp dir
-        item_content = device.get_descendant_by_path(path+item)
+        item_content = device_open.get_descendant_by_path(path+item)
         if item_content is not None:
             mtpy.common_retrieve_to_folder(item_content, tmp_dir_name+item)
             print(f'Copying {item}')
@@ -64,7 +64,6 @@ for item in items_list:
         print(f'Issue copying {item}')
         pass
 print(tmp_dir_name)
-#
-# # device.close()
+device_open.close()
 # # children = device.get_children()
 # # mtpy.common_retrieve_to_folder(children[1], '/tmp/osmand')
