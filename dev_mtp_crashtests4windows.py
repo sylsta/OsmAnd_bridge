8d10e835b3be52c2fd6974db25eb5f5b6e1b4a6f
Sylvain Th?ry <sylvain.thery@cnrs.fr>
2024-11-12 17:27:56 +0100

    refactor

--- dev_mtp_crashtests4windows.py
+++ dev_mtp_crashtests4windows.py
@@ -1,32 +1,43 @@
 import tempfile
+import time
 
 from mtp4windows_win_mtp.access import *
 devices = get_portable_devices()
+
+
+def display_childs_with_walk(dev, root: str) -> None:
+    """Show content of device"""
+    pass
+
+
 for device in devices:
     device_name, device_desc = device.get_description()
     print(f"{device.get_description()[0]} - {device.get_description()[1]}")
+    root_path = None
+    for root, dirs, files in walk(device, device_name):
+       if (len(dirs)) >0:
+           root_path = dirs[0].name
+           break
+    print(root_path)
+    if root_path is None:
+        print("erreur")
+        exit()
 
-for root, dirs, files in (walk(device, device_name)):
-    for directory in dirs:
-        root_path = directory.name
-        break
-    break
+cont = device.get_content()
+#
 potential_paths = ['\\Android\\data\\net.osmand\\files', '\\Android\\data\\net.osmand.plus\\files',
                                            '\\Android\\obb\\net.osmand\\files', '\\Android\\obb\\net.osmand.plus\\files']
 
 path_found = False
 for path in potential_paths:
-    for root, dirs, files in (walk(device, root_path+path)):
-        if len(dirs)>0:
-            path_found = True
-            print(root_path+path)
-            break
-
-    if path_found:
+    if cont.get_path(root_path+path) is not None:
+        path_found = True
         break
-        print(path)
+print(path)
+
 if not path_found:
     # TODO a message box to say no path found
+    print('not_found')
     exit
 tmp_dir_name = tempfile.TemporaryDirectory().name
 
@@ -35,29 +46,31 @@ items_list = ['\\avnotes\\', '\\tracks\\rec\\', '\\favorites\\', '\\itinerary.gp
 os.makedirs(tmp_dir_name + items_list[0])
 os.makedirs(tmp_dir_name + items_list[1])
 os.makedirs(tmp_dir_name + items_list[2])
-cont = device.get_content()
+
 for item in items_list:
     print(path + item)
 
-# try:
-    # copy item to tmp dir
-    if item == '/itinerary.gpx':  # since it's a file not a dir
+# # try:
+#     # copy item to tmp dir
+    if item == '\\itinerary.gpx':  # since it's a file not a dir
         src = root_path + path +item
         dest= tmp_dir_name + item
+        print(f'Copying {src} to {dest}')
         content = cont.get_path(src)  # since it's a file not a dir
+        time.sleep(0.1)
         content.download_file(dest)
     else:
         pass # common_retrieve_to_folder(item_content, tmp_dir_name + item)
     print(f'Copying {item}')
-# except:
-#     print(f'Issue copying {item}')
-#     pass
-    print(tmp_dir_name)
-
-
-
-
-
-
-
-
+# # except:
+# #     print(f'Issue copying {item}')
+# #     pass
+#     print(tmp_dir_name)
+#
+#
+#
+#
+#
+#
+#
+# #
