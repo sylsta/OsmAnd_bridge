d9e0bfd8a024f0753731247448d3c9004a88f70f
Sylvain Théry <sylvain.thery@cnrs.fr>
2025-03-13 16:29:13 +0100

    deleting useless files

--- dev_sandbox.py
+++
@@ -1,7 +0,0 @@
-net_osm_path = r"Smini\Espace de stockage interne partagé\Android\data\net.osmand.plus\files\tiles\Microsoft Earth\\"
-net_osm_path_element= net_osm_path.split('\\')
-path = ''
-for element in net_osm_path_element:
-    path += element + "/"
-    if 'net.osmand' in element:
-        break
